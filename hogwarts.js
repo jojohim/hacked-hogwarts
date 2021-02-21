907e3915c325ab07aee590846699eda3b22234a5
Student-KBH <student-kbh@Student-KBHs-MacBook-Pro.local>
2021-02-21 21:21:29 +0100

    added javascript file and functions

---
+++ hogwarts.js
@@ -0,0 +1,98 @@
+studentsGlobalArray = [];
+let filter;
+
+function start() {
+}
+
+async function loadJSON() {
+}
+
+function displayTable(students) {
+}
+
+function displayStudent(student) {
+}
+
+function cleanStudents(students){
+}
+
+function cleanStudent(student){
+}
+
+function cleanName(fullname){
+}
+
+function hyphenName(name){
+}
+
+function displayData(){
+    displayTable();
+    displayNumbers();
+}
+
+//UNHIDE POPUPS ONCLICK
+function openStudentPopup() {
+}
+
+function openNumberPopup(){
+}
+
+//FILTERS 
+function checkFilter(event) {
+}
+
+function isExpelled(student) {    
+}
+
+function isNotExpelled(student) {    
+}
+
+function isHufflepuff(student){
+}
+
+function isGryffindor(student){
+}
+
+function isSlytherin(student){
+}
+
+function isRavenclaw(student){
+}
+
+function isAll(student){
+}
+
+//SORTING 
+
+//CHECK FOR EXPELL, SQUAD AND PREFECT
+function checkDetails() {
+    //toggle expell to make expelled true and false 
+toggleExpell();
+    //toggle squad member 
+toggleSquadMember();
+    //toggle prefect
+togglePrefect();
+}
+
+function toggleExpell(){
+}
+
+function toggleSquadMember(){
+    //if state false add as squad member 
+    //if state true delete as squad member
+} 
+
+function togglePrefect(){
+    //deletes last prefect and adds new one
+}
+
+//NUMBERS
+function displayNumbers(){
+}
+
+function getNumbers(){
+}
+
+//HACKING
+
+
